c779f632c6b451fc9d9b8ebfc2efd2bde90777fb
legax-creator <my3reeder@gmail.com>
2026-07-11 21:05:18 +0300

    Create KarmaMod.java

---
+++ src/main/KarmaMod.java
@@ -0,0 +1,22 @@
+package com.karma.karmamod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(KarmaMod.MODID)
+public class KarmaMod {
+    public static final String MODID = "karmamod";
+
+    public static void init() {
+        // Alt sistemlerin ve network paketlerinin kaydı
+        NetworkHandler.register();
+    }
+
+    public KarmaMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        init();
+    }
+}
